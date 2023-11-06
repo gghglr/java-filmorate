44b3907ce421e92e178fdd5ab690615b31b18c80
Alexey <aleshka-yurlov@yandex.ru>
2023-11-06 10:58:39 +0300

    feat: test

--- src/test/java/ru/yandex/practicum/filmorate/Controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/Controller/FilmControllerTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.Exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.User;
 
 import java.time.LocalDate;
 
