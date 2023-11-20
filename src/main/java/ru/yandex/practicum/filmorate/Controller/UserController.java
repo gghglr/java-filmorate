334e2bee3dcff605f5d4f37014348777b73ba33c
Alexey <aleshka-yurlov@yandex.ru>
2023-11-21 00:49:04 +0300

    fix

--- src/main/java/ru/yandex/practicum/filmorate/Controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/Controller/UserController.java
@@ -20,7 +20,7 @@ public class UserController {
 
     @Autowired
     public UserController(UserService userService) {
-        this.userService =userService;
+        this.userService = userService;
         inMemoryUserStorage = userService.getStorage();
     }
 
